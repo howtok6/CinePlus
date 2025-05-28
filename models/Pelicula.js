7871e7a586612a78a321481259f3fcec4fa9c5a4
howtok6 <you@example.com>
2025-05-28 18:35:59 -0400

    fix api

--- models/Pelicula.js
+++ models/Pelicula.js
@@ -3,9 +3,6 @@ const mongoose = require('mongoose');
 const peliculaSchema = new mongoose.Schema({
   titulo: String,
   imagen: String,
-  descripcion: String,
-  categoria: String,
-  estreno: Date
 });
 
 module.exports = mongoose.model('Pelicula', peliculaSchema);
